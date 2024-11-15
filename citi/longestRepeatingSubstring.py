124cd739f06f86f18a76ffe5280c2ed24c49dd92
2011B4A7584P <ashish.dalal.bitspilani@gmail.com>
2024-11-15 10:09:38 +0530

    longest_repeating_substring

--- citi/longestRepeatingSubstring.py
+++ citi/longestRepeatingSubstring.py
@@ -1,17 +1,20 @@
-# s = "ashish is a developer. ashish is appearing in an interview."
-# "longest repeating word"
-from collections import Counter
-def longest_repeating_word(s:str)->int:
-    word_counter = dict(Counter(s))
-    return max(list(word_counter.keys()), key=len)
+# s = "ashish is a developer.ashish is appearing in an interview."
 
-def longest_repeating_substring(s:str)->int:
-    freq = {}
-    freq[tuple(substring, len)] = frequency
+from collections import defaultdict
+
+def longest_repeating_substring(string:str)->(str, int):
+    left, right, counter, max_len, result = 0, 0, defaultdict(int), -1, ""
+    for left in range(len(string)):
+        for right in range(left+1, len(string)+1):
+            substring = string[left:right]
+            counter[substring] +=  1
+            length = right - left
+
+            if max_len < length and counter[substring] > 1:
+                max_len, result = length, substring
+
+    return result, max_len
 
 
-try:
 
-except:
-
-finally:
+print(longest_repeating_substring(string = "ashish is a developer.ashish is appearing in an interview."))
